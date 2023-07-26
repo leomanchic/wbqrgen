75f5fda106385f336f6c13da0c24d97694884cd5
leomanchic <perleomix@gmail.com>
2023-07-27 02:31:51 +0300

    size param added

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,23 @@ use std::path::Path;
 use std::io::{self, BufRead};
 use clap::Parser;
 
+use derivative::Derivative;
+
+
+
 #[derive(Parser)]
+#[derive(Derivative)]
 struct CLI{
     /// The pattern to look for
     #[arg(short = 'e', long = "extention")]
     pattern: String,
-    /// The path to the file to read
+    /// The path to the file to read from
     #[arg(short = 'f', long = "file")]
     path: std::path::PathBuf,
+    ///Size of an output image in px^2
+    #[arg(short = 's', long = "size")]
+    #[derivative(Default(value = "500"))]
+    size: usize,
 }
 
 
@@ -20,7 +29,7 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = match  File::open(filename){
         Ok(f) => Ok(io::BufReader::new(f).lines()),
-        Err(_) => panic!("Error while reading file"),
+        Err(_) => panic!("Error while reading file!!!"),
     };
     file
 }
@@ -32,14 +41,16 @@ fn main() {
         // Получает итератор, который возвращает Option
         for line in lines {
             if let Ok(product) = line {
+                //Определить расширение файла
                 let mut string = product[..].to_string();
+                // Переписать под match, пока под if
                 if &args.pattern == "svg"{
                     &string.push_str(".svg");
-                    qrcode_generator::to_svg_to_file(&product, QrCodeEcc::Low, 500,None::<&str>, &string).unwrap();
+                    qrcode_generator::to_svg_to_file(&product, QrCodeEcc::Low,args.size,None::<&str>, &string).unwrap();
                 };
                 if &args.pattern == "png"{
                     &string.push_str(".png");
-                    qrcode_generator::to_png_to_file(product, QrCodeEcc::Low, 500, string).unwrap();
+                    qrcode_generator::to_png_to_file(product, QrCodeEcc::Low, args.size, string).unwrap();
                 };
                 
                 
