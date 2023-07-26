bd13b2e6ab4d0f0658ed0b3db3fb4eaf8d9c2a2c
leomanchic <perleomix@gmail.com>
2023-07-27 01:16:28 +0300

    first

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use qrcode_generator::QrCodeEcc;
+use std::fs::File;
+use std::path::Path;
+use std::io::{self, BufRead};
+use clap::Parser;
+
+
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = match  File::open(filename){
+        Ok(f) => Ok(io::BufReader::new(f).lines()),
+        Err(_) => panic!("Error while reading file"),
+    };
+    file
+}
+
+
+fn main() {
+    if let Ok(lines) = read_lines("./wbqr.txt") {
+        // Получает итератор, который возвращает Option
+        for line in lines {
+            if let Ok(product) = line {
+                let mut string = product[..].to_string();
+                string.push_str(".png");
+                qrcode_generator::to_png_to_file(product, QrCodeEcc::Low, 500, string).unwrap();
+            }      
+        }   
+    }
+      
+}
+
+// let chars_to_trim: &[char] = &['\n'];
+    // let trimmed_str: &str = file_to_readfrom.trim_matches(chars_to_trim);
+    // for i in file_to_readfrom.re
+    // 
